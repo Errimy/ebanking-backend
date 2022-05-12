456ee726b9271df52172011ad66bf3cdcf417184
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-05-12 21:33:07 +0100

    Mapping DONE.

--- src/main/java/com/example/ebankingbackend/entities/BankAccount.java
+++ src/main/java/com/example/ebankingbackend/entities/BankAccount.java
@@ -5,15 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
+@Entity
 @Data
 @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
+    @Id
     private String id;
     private double balance;
     private Date createdAt;
     private AccountStatus status;
+    @ManyToOne
     private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
     private List<AccountOperation> accountOperationList;
 }
