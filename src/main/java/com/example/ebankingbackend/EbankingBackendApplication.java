67ab29cc2af858cb63694b819861dbef05b5000d
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-05-12 23:36:03 +0100

    Getting details on bank account with ID

--- src/main/java/com/example/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/com/example/ebankingbackend/EbankingBackendApplication.java
@@ -1,9 +1,6 @@
 package com.example.ebankingbackend;
 
-import com.example.ebankingbackend.entities.AccountOperation;
-import com.example.ebankingbackend.entities.CurrentAccount;
-import com.example.ebankingbackend.entities.Customer;
-import com.example.ebankingbackend.entities.SavingAccount;
+import com.example.ebankingbackend.entities.*;
 import com.example.ebankingbackend.enums.AccountStatus;
 import com.example.ebankingbackend.enums.OperationType;
 import com.example.ebankingbackend.repositories.AccountOperationRepository;
@@ -25,6 +22,32 @@ public class EbankingBackendApplication {
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
     @Bean
+    CommandLineRunner commandLineRunner(BankAccountRepository bankAccountRepository){
+
+
+        return args -> {
+            BankAccount bankAccount= bankAccountRepository.findById("3966c616-e554-458f-8013-ba17c26d9198").orElse(null);
+            if(bankAccount!= null){
+                System.out.println("******************************");
+                System.out.println(bankAccount.getId());
+                System.out.println(bankAccount.getBalance());
+                System.out.println(bankAccount.getStatus());
+                System.out.println(bankAccount.getCreatedAt());
+                System.out.println(bankAccount.getCustomer().getName());
+                System.out.println(bankAccount.getClass().getSimpleName());
+                if (bankAccount instanceof CurrentAccount){
+                    System.out.println("Overdraft:" +((CurrentAccount) bankAccount).getOverDraft());
+                }
+                else if (bankAccount instanceof SavingAccount){
+                    System.out.println("Rate:" +((SavingAccount) bankAccount).getInterestRate());
+                }
+                bankAccount.getAccountOperationList().forEach(accountOperation -> {
+                    System.out.println(accountOperation.getType()+"\t"+accountOperation.getOperationDate()+"\t"+accountOperation.getAmount());
+                });
+            }
+        };
+    }
+    //@Bean
     CommandLineRunner start(CustomerRepository customerRepository,
                             BankAccountRepository bankAccountRepository,
                             AccountOperationRepository accountOperationRepository){
