8fddca0b93c12e96fb29e42dff8187e1cd9de9f0
Hatim Errimy <hatim-s.o.s@outlook.fr>
2022-05-12 22:01:21 +0100

    Created Repos And Added Customers To Database.

--- src/main/java/com/example/ebankingbackend/EbankingBackendApplication.java
+++ src/main/java/com/example/ebankingbackend/EbankingBackendApplication.java
@@ -1,7 +1,15 @@
 package com.example.ebankingbackend;
 
+import com.example.ebankingbackend.entities.Customer;
+import com.example.ebankingbackend.repositories.AccountOperationRepository;
+import com.example.ebankingbackend.repositories.BankAccountRepository;
+import com.example.ebankingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EbankingBackendApplication {
@@ -9,5 +17,17 @@ public class EbankingBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository){
+        return args -> {
+            Stream.of("Hatim","Ismail","Mouad").forEach(name->{
+                Customer customer= new Customer();
+                customer.setName(name);
+                customer.setEmail(name+"gmail.com");
+                customerRepository.save(customer);
+            });
+        };
+    }
 }
